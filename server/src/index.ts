60bc8a0716e5df9e253ee32c5cfa47313ae900dd
Maher-Majdoub <maher.majjdoub@gmail.com>
2024-09-18 17:41:12 +0100

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
